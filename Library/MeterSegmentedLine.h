d8547b7560cca6a3d3e3dba4688d86476cdb8426
shengd <dsheng@utexas.edu>
2014-10-19 21:24:41 -0700

    Added missing files from name change

---
+++ Library/MeterSegmentedLine.h
@@ -0,0 +1,42 @@
+#ifndef __METERSEGMENTEDLINE_H__
+#define __METERSEGMENTEDLINE_H__
+
+#include "Meter.h"
+
+class MeterSegmentedLine : public Meter
+{
+public:
+	MeterSegmentedLine(Skin* skin, const WCHAR* name);
+	virtual ~MeterSegmentedLine();
+
+	MeterSegmentedLine(const MeterSegmentedLine& other) = delete;
+	MeterSegmentedLine& operator=(MeterSegmentedLine other) = delete;
+
+	virtual UINT GetTypeID() { return TypeID<MeterSegmentedLine>(); }
+
+	virtual void Initialize();
+	virtual bool Update();
+	virtual bool Draw(Gfx::Canvas& canvas);
+
+protected:
+	virtual void ReadOptions(ConfigParser& parser, const WCHAR* section);
+	virtual void BindMeasures(ConfigParser& parser, const WCHAR* section);
+
+private:
+	static std::vector<Gdiplus::Color> defColor;
+
+	std::vector<std::vector<Gdiplus::Color>> m_Colors;	//outer vector is series, inner vector is segments
+	std::vector<double> m_ScaleValues;
+
+	std::vector<INT> m_Segments;
+
+	bool m_Autoscale;
+	double m_LineWidth;
+
+	std::vector<std::vector<double>> m_AllValues;		//outer vector is series, inner vector is data points
+	int m_CurrentPos;
+
+	bool m_GraphStartLeft;
+};
+
+#endif
