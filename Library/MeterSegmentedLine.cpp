33862dc5ba387114356cfeea58ea43f997a4a1d3
shengd <dsheng@utexas.edu>
2014-10-24 18:42:18 -0700

    Enabled slower update speeds

--- Library/MeterSegmentedLine.cpp
+++ Library/MeterSegmentedLine.cpp
@@ -340,7 +340,7 @@ bool MeterSegmentedLine::Draw(Gfx::Canvas& canvas)
 		{
 			for (int j = meterRect.X + 1, R = meterRect.X + meterRect.Width; j < R; ++j)
 			{
-				++pos;
+				pos += m_SegmentDividers[segmentInd];
 				pos %= meterRect.Width;
 
 				calcY(Y);
@@ -361,7 +361,7 @@ bool MeterSegmentedLine::Draw(Gfx::Canvas& canvas)
 		{
 			for (int j = meterRect.X + meterRect.Width, R = meterRect.X + 1; j > R; --j)
 			{
-				++pos;
+				pos += m_SegmentDividers[segmentInd];
 				pos %= meterRect.Width;
 
 				calcY(Y);
